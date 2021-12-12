1935f9e5625d799f38a0729a027423b49ab0ce60
piaojing <vvv198869@gmail.com>
2021-12-12 19:46:39 +0700

    React-parents class to function, child funtioin to class

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React, { Component, useState } from 'react';
 import './App.css';
 
-function App() {
+import UserInput from './UserInput/UserInput';
+import UserOutput from './UserOutput/UserOutput';
+
+const App=(props)=> {
+
+  const [name, setName]= useState('supermax')
+
+  const usernameChangedHandler = (event) => {
+    setName(event.target.value)};
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ol>
+        <li>Create TWO new components: UserInput and UserOutput</li>
+        <li>UserInput should hold an input element, UserOutput two paragraphs</li>
+        <li>Output multiple UserOutput components in the App component (any paragraph texts of your choice)</li>
+        <li>Pass a username (of your choice) to UserOutput via props and display it there</li>
+        <li>Add state to the App component (=> the username) and pass the username to the UserOutput component</li>
+        <li>Add a method to manipulate the state (=> an event-handler method)</li>
+        <li>Pass the event-handler method reference to the UserInput component and bind it to the input-change event</li>
+        <li>Ensure that the new input entered by the user overwrites the old username passed to UserOutput</li>
+        <li>Add two-way-binding to your input (in UserInput) to also display the starting username</li>
+        <li>Add styling of your choice to your components/ elements in the components - both with inline styles and stylesheets</li>
+      </ol>
+      <UserInput 
+        changed={usernameChangedHandler} 
+        currentName={name} />
+      <UserOutput userName={name} />
+      <UserOutput userName={name} />
+      <UserOutput userName="Max" />
     </div>
   );
 }
---
+++ src/UserInput/UserInput.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+class userInput extends React.Component {
+  constructor(props) {
+    // fires before component is mounted
+    super(props);
+    this.state = {
+        inputStyle : {
+                    border: '2px solid red'
+                },
+    };
+  }
+
+  
+  render() {
+    return (
+      <input
+        type="text"
+        style={this.state.inputStyle}
+        onChange={this.props.changed}
+        value={this.props.currentName}
+      />
+    );
+  }
+}
+
+// const userInput = (props) => {
+//     const inputStyle = {
+//         border: '2px solid red'
+//     };
+
+//     return <input
+//         type="text"
+//         style={inputStyle}
+//         onChange={props.changed}
+//         value={props.currentName} />;
+// };
+
+export default userInput;
